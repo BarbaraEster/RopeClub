5c0e1e7a764a6d391fb5395cc0b399b2416522a6
Barbara Ester <lineage2.forever@hotmail.com>
2016-04-18 13:08:07 -0300

    Commit Projeto Inteiro

---
+++ ProjetoRopeClub/Assets/Scripts/Player1.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player1 : MonoBehaviour {
+
+	float Forca;
+	Vector3 posicionamento;
+	// Use this for initialization
+	void Start () {
+		Forca = 0.1f;
+		posicionamento = transform.position;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.A)) 
+		{
+			transform.Translate (-Forca, 0, 0);
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow)) 
+		{
+			transform.Translate (Forca, 0, 0);
+		}
+	
+	}
+}
